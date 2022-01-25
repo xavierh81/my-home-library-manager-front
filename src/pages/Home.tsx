00559b52df2ead2b9a9be19cb73c5e56d43b2329
Xavier HEYSCH <xavier@codefever.fr>
2022-01-25 12:02:56 +0100

    Show user on homepage + readme update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 // Imports
-import React from 'react';
+import React, { useEffect } from 'react';
 
-// Apollo / GraphQL
-
+// Apollo
+import { useApolloClient } from '@apollo/client';
+import { getUser } from 'graphql/api';
 
 // Components
 import MainLayout from 'components/templates/MainLayout';
@@ -18,14 +19,27 @@ type HomeState = {
 
 // Main element
 function Home() {
+    // Load hooks
+    const apolloClient = useApolloClient();
+
     // Define state variables
     const [state, setState] = React.useState<HomeState>({ user: null });
+
+    // Run this at the page mount
+    useEffect(() => {
+        // Retrieve user
+        getUser(apolloClient).then((user) => {
+            setState(prevState => ({ ...prevState, user}))
+        })
+        .catch((error) => {
+
+        })
+    }, [apolloClient])
 
     // Render
     return (
         <MainLayout>
-            Test home layout
-            {state.user != null && <span>{state.user.name}</span>}
+            <h1>Welcome {state.user != null && <span>{state.user.firstName}</span>}</h1>
         </MainLayout>
     )
 }
